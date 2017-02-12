20d92f465cc630a76d199702e25c59625ceebe87
cain19811028 <cain19811028@gmail.com>
2017-02-12 17:56:28 +0800

    補充 README.md 和 BinaryGap 的題目說明

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python_codility
+
+使用 Python 練習 Codility 的題目
+Codility：https://codility.com/programmers/
+
+| Date | Lesson | Task |
+| ------ | ------ | ------ |
+| 20170212 | Lesson 1 - Iterations | BinaryGap |
--- python_binary_gap.py
+++ python_binary_gap.py
@@ -3,11 +3,22 @@ import re
 
 """
 題目：BinaryGap
+－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
 For example, number 9 has binary representation 1001 and contains a binary gap of length 2.
 The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
-The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+The number 20 has binary representation 10100 and contains one binary gap of length 1.
+The number 15 has binary representation 1111 and has no binary gaps.
+
+Write a function:
+
+int solution(int N);
+that, given a positive integer N, returns the length of its longest binary gap.
+The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5,
+because N has binary representation 10000010001 and so its longest binary gap is of length 5.
 """
 
 N = 328
